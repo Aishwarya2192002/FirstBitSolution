ec6028afeeda7f00ae59a756cbb6e511547f3770
Aishwarya2192002 <aishwaryamane2121@gmail.com>
2025-08-08 09:11:51 +0530

    Add files via upload

---
+++ C module/Assignment 0/Assignment5/Q1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int no;
+    printf("Enter number:");
+    scanf("%d", &no);
+    for ( int i = 1; i <=no; i++)
+    {
+        for ( int j = 1; j<=no; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
