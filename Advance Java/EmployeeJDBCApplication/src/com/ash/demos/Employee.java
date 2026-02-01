b27fcaab7da5245be347796b0565537a21a678a7
Aishwarya2192002 <aishwaryamane2121@gmail.com>
2026-02-01 16:07:51 +0530

    Add files via upload

---
+++ Advance Java/EmployeeJDBCApplication/src/com/ash/demos/Employee.java
@@ -0,0 +1,38 @@
+package com.ash.demos;
+
+public class Employee {
+
+	int eId;
+	String eName;
+	double eSalary;
+	public Employee(int eId, String eName, double eSalary) {
+		super();
+		this.eId = eId;
+		this.eName = eName;
+		this.eSalary = eSalary;
+	}
+	public int geteId() {
+		return eId;
+	}
+	public void seteId(int eId) {
+		this.eId = eId;
+	}
+	public String geteName() {
+		return eName;
+	}
+	public void seteName(String eName) {
+		this.eName = eName;
+	}
+	public double geteSalary() {
+		return eSalary;
+	}
+	public void seteSalary(double eSalary) {
+		this.eSalary = eSalary;
+	}
+	@Override
+	public String toString() {
+		return "Employee : \n Employee Id=" + eId + "\n Employee Name=" + eName + "\n Employee Salary=" + eSalary + "";
+	}
+	
+	
+}
